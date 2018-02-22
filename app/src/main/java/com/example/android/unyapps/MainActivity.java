961e32c7b2a3fda399e8c682a2d299e0315994b5
aisardotkom <fahmiaisar99@gmail.com>
2018-02-22 20:38:33 +0700

    tri

--- app/src/main/java/com/example/android/unyapps/MainActivity.java
+++ app/src/main/java/com/example/android/unyapps/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         sp = getSharedPreferences("com.example.android.unyapps", MODE_PRIVATE);
@@ -60,19 +60,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity
 
         protected void onPostExecute(Void result) {
             // Set title into TextView
-            TextView titleText = (TextView) findViewById(R.id.titleText);
+            TextView titleText = findViewById(R.id.titleText);
             TextView postTitleText = findViewById(R.id.postTitleText);
             LinearLayout newsView = findViewById(R.id.newsView);
             int index = 0;
