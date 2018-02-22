e20361bac1fda579fe1b610130ab7d46adcf81ec
aisardotkom <fahmiaisar99@gmail.com>
2018-02-22 21:23:10 +0700

    mantab

--- app/src/main/java/com/example/android/unyapps/MainActivity.java
+++ app/src/main/java/com/example/android/unyapps/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         sp = getSharedPreferences("com.example.android.unyapps", MODE_PRIVATE);
