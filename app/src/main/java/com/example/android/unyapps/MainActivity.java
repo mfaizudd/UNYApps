3c86dd79a6da728473979caa4277542356f299fc
Fafa <mfaizudd@gmail.com>
2018-02-22 20:16:26 +0700

    First Commit

---
+++ app/src/main/java/com/example/android/unyapps/MainActivity.java
@@ -0,0 +1,202 @@
+package com.example.android.unyapps;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Text;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    String url = "https://uny.ac.id/index-berita";
+    ProgressDialog mProgressDialog;
+    String postTitle[] = new String[10];
+    Button loadButton;
+    String postLink[] = new String[10];
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        sp = getSharedPreferences("com.example.android.unyapps", MODE_PRIVATE);
+
+        loadButton = findViewById(R.id.loadButton);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Title().execute();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private class Title extends AsyncTask<Void, Void, Void> {
+        String title;
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("UNY Apps");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).get();
+                // Get the html document title
+                Elements img = document.select("a[title=Home] img[src]");
+                Elements epostTitle = document.select("strong[class=field-content] a[href]");
+                int index = 0;
+                for(Element item : epostTitle) {
+                    postTitle[index] = item.ownText();
+                    postLink[index] = item.attr("href");
+                    index++;
+                    if(index>=10) break;
+                }
+                String imgSrc = img.attr("src");
+                InputStream input = new java.net.URL(imgSrc).openStream();
+                bitmap = BitmapFactory.decodeStream(input);
+                title = document.title();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            // Set title into TextView
+            TextView titleText = (TextView) findViewById(R.id.titleText);
+            TextView postTitleText = findViewById(R.id.postTitleText);
+            LinearLayout newsView = findViewById(R.id.newsView);
+            int index = 0;
+            for(String item : postTitle) {
+                Button mButton = new Button(getApplicationContext());
+                mButton.setText(item);
+                final int indexFinal = index;
+                final String itemFinal = item;
+                mButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        SharedPreferences.Editor edit = sp.edit();
+                        edit.putString("postLink", postLink[indexFinal]);
+                        edit.putString("postTitle", itemFinal);
+                        edit.apply();
+                        startActivity(new Intent(MainActivity.this, PostViewActivity.class));
+                    }
+                });
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                newsView.addView(mButton, lp);
+                index++;
+            }
+            titleText.setText(title);
+            ImageView logoImage = findViewById(R.id.logoImage);
+            ImageView drawerLogo = findViewById(R.id.imageView);
+            drawerLogo.setImageBitmap(bitmap);
+            logoImage.setImageBitmap(bitmap);
+            mProgressDialog.dismiss();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/unyapps/PostViewActivity.java
@@ -0,0 +1,95 @@
+package com.example.android.unyapps;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class PostViewActivity extends AppCompatActivity {
+
+    ProgressDialog mProgressDialog;
+    SharedPreferences sp;
+    String url = "";
+    String title = "";
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_view);
+
+        new ProcessData().execute();
+
+        tv = findViewById(R.id.postViewContent);
+
+        sp = getSharedPreferences("com.example.android.unyapps", MODE_PRIVATE);
+        title = sp.getString("postTitle", "");
+        url = "http://www.uny.ac.id"+sp.getString("postLink", "");
+//        tv.setText(url);
+    }
+
+    private class ProcessData extends AsyncTask<Void, Void, Void> {
+        String title;
+        Bitmap bitmap;
+        String content;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(PostViewActivity.this);
+            mProgressDialog.setTitle("UNY Apps");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).get();
+                Elements epostTitle = document.select("h1[class=title] a[href]");
+                title = epostTitle.text();
+                Elements img = document.select("a[class=colorbox] img[src]");
+                InputStream is = new java.net.URL(img.attr("src")).openStream();
+                bitmap = BitmapFactory.decodeStream(is);
+                Elements econtent = document.select("div[class=field-item even] p");
+                content = econtent.text();
+                // Get the html document title
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set title into TextView
+            TextView titleTextView = findViewById(R.id.postViewTitleText);
+            titleTextView.setText(title);
+            ImageView postImage = findViewById(R.id.postImage);
+            postImage.setImageBitmap(bitmap);
+            TextView textContent = findViewById(R.id.postViewContent);
+            textContent.setText(content);
+            mProgressDialog.dismiss();
+        }
+    }
+}
